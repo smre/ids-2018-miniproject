6701a5d153acdd9cad7b76105754e488abe1b3d4
sumire <sumire@mailbox.org>
2018-10-23 15:15:08 +0300

    Fetch JSON data from API

--- website/website/static/main.js
+++ website/website/static/main.js
@@ -1,55 +1,120 @@
+const getData = url => {
+    return axios.get(`/api/${url}`)
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (error) {
+            return error;
+        });
+};
+
+const createHeatmapLayer = () => {
+    return getData('ticketAmounts')
+        .then(points => {
+            let heatArray = [];
+            points.data.map(marker => {
+                if (marker.latitude !== 'FAIL') {
+                    heatArray.push([marker.latitude, marker.longitude, marker.amount])
+                }
+            });
+            // Heatmap options can be set here
+            return L.heatLayer(heatArray, {blur: 5, max: 726 / 726, radius: 10});
+        });
+};
+
+const createTicketLayer = () => {
+    return getData('ticketAmounts')
+        .then(points => {
+            let ticketArray = [];
+            points.data.map(marker => {
+                if (marker.latitude !== 'FAIL') {
+                    ticketArray.push(L.circleMarker(L.latLng(marker.latitude, marker.longitude), {
+                        radius: 2,
+                        color: "#D0021B",
+                        weight: 1,
+                        fillOpacity: 1
+                    }));
+                }
+            });
+            return ticketArray;
+        });
+};
+
+const createParkingSpotLayer = () => {
+    return getData('parking')
+        .then(parkingSpots => {
+            let parkingArray = [];
+            parkingSpots.data.map(p => {
+                parkingArray.push(L.geoJSON(p, {color: "#417505", weight: 2}));
+            });
+            return parkingArray;
+        });
+};
+
+const createServiceClustersLayer = () => {
+    return getData('serviceClusters')
+        .then(clusters => {
+            let serviceClusterArray = [];
+            clusters.data.map(p => {
+                serviceClusterArray.push(L.circleMarker(L.latLng(p.latitude, p.longitude), {
+                    radius: (p.count / 6) + 2,
+                    color: "#00DDDD",
+                    weight: 1,
+                    fillOpacity: 1
+                }));
+            });
+            return serviceClusterArray;
+        });
+};
+
+// Initialize map
 // Leaflet Map object
 const map = L.map("map").setView([60.1691, 24.9415], 13);
 
 // Add OSM as map source
 const basemap = L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors and <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
 }).addTo(map);
 
+function createLayers(base) {
+    let baseMaps = {
+        "Basemap": base,
+    };
 
-// Create a heatmap layer
-let heatArray = [];
-let points = getData();
-points.map(marker => heatArray.push([marker.latitude, marker.longitude, marker.amount]));
-// Heatmap options
-const heat = L.heatLayer(heatArray, {blur: 5, max: 30, radius: 7});
+    let overlayMaps = {};
 
-// Add markers to a layer
-let ticketArray = [];
-points.map(marker => {
-    ticketArray.push(L.circleMarker(L.latLng(marker.latitude, marker.longitude), { radius: 2, color: "#D0021B", weight: 1, fillOpacity: 1 }));
-});
+    let layersControl = L.control.layers(baseMaps, overlayMaps);
+    layersControl.addTo(map);
 
-// Add parking spots to a layer
-let parkingArray = [];
-let parkingSpots = getParking();
-parkingSpots.map(p => {
-    parkingArray.push(L.geoJSON(p, { color: "#417505", weight: 2 }));
-});
+    // Add layers as are fetched from API
+    createTicketLayer().then(data => {
+        layersControl.remove(map);
+        overlayMaps['Tickets'] = L.layerGroup(data);
+        layersControl = L.control.layers(baseMaps, overlayMaps);
+        layersControl.addTo(map);
+    });
 
-let serviceClusterArray = [];
-let clusters = getServiceClusters();
-clusters.map(p => {
-	serviceClusterArray.push(L.circleMarker(L.latLng(p.latitude, p.longitude), {radius: (p.count/6)+2, color: "#00DDDD", weight: 1, fillOpacity: 1}));
-});
+    createHeatmapLayer().then(data => {
+        layersControl.remove(map);
+        overlayMaps['Ticket Heatmap'] = data;
+        layersControl = L.control.layers(baseMaps, overlayMaps);
+        layersControl.addTo(map);
+    });
 
-let heatmapLayer = L.layerGroup([heat]);
-let ticketsLayer = L.layerGroup(ticketArray);
-let parkingLayer = L.layerGroup(parkingArray);
-let serviceClusterLayer = L.layerGroup(serviceClusterArray);
+    createParkingSpotLayer().then(data => {
+        layersControl.remove(map);
+        overlayMaps['Parking Spots'] = L.layerGroup(data);
+        layersControl = L.control.layers(baseMaps, overlayMaps);
+        layersControl.addTo(map);
+    });
 
-const baseMaps = {
-    //"Basemap": basemap,
-};
+    createServiceClustersLayer().then(data => {
+        layersControl.remove(map);
+        overlayMaps['Service Clusters'] = L.layerGroup(data);
+        layersControl = L.control.layers(baseMaps, overlayMaps);
+        layersControl.addTo(map);
+    });
+}
 
-const overlayMaps = {
-    "Tickets": ticketsLayer,
-    "Heatmap": heatmapLayer,
-    "Parking": parkingLayer,
-    "ServiceClusters": serviceClusterLayer
-};
-
-
-
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+createLayers(basemap);
--- website/website/website.py
+++ website/website/website.py
@@ -1,9 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import csv
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def frontpage():
     return render_template('index.html')
+
+
+# API
+@app.route('/api/ticketAmounts')
+def send_ticket_amounts():
+    json_data = open('data/ticket_amounts.json', encoding="UTF-8").read()
+    data = json.loads(json_data)
+    return jsonify(data)
+
+
+@app.route('/api/parking')
+def send_parking():
+    json_data = open('data/parking.json', encoding="UTF-8").read()
+    data = json.loads(json_data)
+    return jsonify(data)
+
+
+@app.route('/api/serviceClusters')
+def send_service_clusters():
+    json_data = open('data/service_clusters.json', encoding="UTF-8").read()
+    data = json.loads(json_data)
+    return jsonify(data)
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
